814d117b85787901ba4f733e5cd80ce5115635ca
Juan Maldonado <juan.maldonado@gmail.com>
2014-04-27 21:57:21 +0000

    Almost final

--- css/index.php
+++ css/index.php
@@ -30,38 +30,34 @@
   <section id="main_content"> 
 
     <header id="main">
-      <h2>This is where content goes</h2>
-    <p>Since we want the eye to be able to read clearly, this section should be clean and full of white space. Below are the articles associated with the site. 
+      <h2>HTML and CSS sound great, but how does one get started designing web sites?</h2>
+      <p>That's a very good question. There are a number of excellent programs with graphical user interfaces that allow you to design web sites. Some are so powerful that you don't even need to touch any HTML or CSS code directly, and they are even hosted online so file transfers aren't necessary. These are called CMSes, or content management systems. An example of a powerful one is <a href="http://wordpress.org">WordPress</a>.</p>
+      <p>However, that's not a great way to learn how to code. Below are some good tools to get started.</p> 
     </header>
 
     <article id="first">
-    <h3>Corporate Ipsum</h3>
-    <p>Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI.</p>
-
-    <p>Efficiently unleash cross-media information without cross-media value. Quickly maximize timely deliverables for real-time schemas. Dramatically maintain clicks-and-mortar solutions without functional solutions.</p>
-
-    <p>Completely synergize resource sucking relationships via premier niche markets. Professionally cultivate one-to-one customer service with robust ideas. Dynamically innovate resource-leveling customer service for state of the art customer service.</p></article>
+      <h3>Software required</h3>
+      <p>A plain text editor, which all operating systems include by default. Seriously, that's it! HTML and CSS documents are plain text, so as long as you can generate plain text files and give them .html and .css extensions, you're already off to a great start. Below are some good text editors by operating system:</p>
+      <ul>
+        <li><span class="bold">Windows</span> - Notepad</li>
+        <li><span class="bold">Mac</span> - TextEdit</li>
+        <li><span class="bold">Unix</span> - gedit for the desktop, vi or nano for the command line interface (your instructor used vi to make this page, by the way)</li>
+      </ul>
+    </article>
 
     <article id="second">
-    <h3>More corporate ipsum</h3>
-
-    <p>Objectively innovate empowered manufactured products whereas parallel platforms. Holisticly predominate extensible testing procedures for reliable supply chains. Dramatically engage top-line web services vis-a-vis cutting-edge deliverables.</p>
-
-    <p>Proactively envisioned multimedia based expertise and cross-media growth strategies. Seamlessly visualize quality intellectual capital without superior collaboration and idea-sharing. Holistically pontificate installed base portals after maintainable products.<p>
-
-    <p>Phosfluorescently engage worldwide methodologies with web-enabled technology. Interactively coordinate proactive e-commerce via process-centric "outside the box" thinking. Completely pursue scalable customer service through sustainable potentialities.</p>
- 
+      <h3>The web page is complete! How do I show it off to the world?</h3>
+      <p>You have to transfer your finished web page files to a web server. There are a number of good, inexpensive web hosts out there:</p>
+      <ul>
+        <li><span class="bold">Amazon Web Services</span> - AWS can have a full hosting environment in no time and they have a "pay as you go" model. If your web site doesn't get a whole lot of traffic, you may not even pay for service.</li>
+        <li><span class="bold">Digital Ocean</span> - Digital Ocean can have a full hosting environment up and running in less than one minute, and there are many operating systems to choose from. This site is hosted in San Francisco on a server running Ubuntu Linux as the operating system.</li> 
+      </ul>
     </article>
 
     <article id="third">
-    <h3>And finally, the last of the corporate ipsum</h3>
-
-    <p>Collaboratively administrate turnkey channels whereas virtual e-tailers. Objectively seize scalable metrics whereas proactive e-services. Seamlessly empower fully researched growth strategies and interoperable internal or "organic" sources.</p>
-
-    <p>Credibly innovate granular internal or "organic" sources whereas high standards in web-readiness. Energistically scale future-proof core competencies vis-a-vis impactful experiences. Dramatically synthesize integrated schemas with optimal networks.</p>
-
-    <p>Interactively procrastinate high-payoff content without backward-compatible data. Quickly cultivate optimal processes and tactical architectures. Completely iterate covalent strategic theme areas via accurate e-markets.</p>
-
+      <h3>My web page has a home. How do I get the files up there?</h3>
+      <p>The tried and true method for getting files onto a web server is by using an FTP client. FTP stands for File Transfer Protocol. The first image online was transferred to a web server using FTP, and the person doing the transferring didn't even know what a web page was! The most popular FTP client is called <a href="https://filezilla-project.org/">FileZilla</a> and is available for most major operating systems.</p>
+      <p>An increasingly popular option is to use an existing feature in versioning software. A popular one is <a href="http://github.com">Git</a> which allows you to not only save your work and revisit older versions as needed, but it also allows you to save your work to the cloud and even work collaboratively with other designers. You can log in to your web server and run a Git command that will download the entire contents of your web page straight to the web server.</p>
     </article>
 
   </section>
