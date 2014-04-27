a767d8635782be507765ae4b6b3265c16e4362e6
root <root@nenasknittings.com>
2014-04-27 19:52:08 +0000

    First iteration

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">   
+    <title>Welcome to Introduction to HTML and CSS</title>
+    <link rel="stylesheet" type="text/css" href="css/css/style.css">
+</head>
+
+<body>
+
+  <header id="body">
+    <h1>Intro to HTML and CSS</h1>
+    <aside>Juan Maldonado, instructor</aside>
+  </header>
+
+  <footer>
+    <section id="footer_content">
+      Teaching demo for Portland Community College, Rock Creek Campus - April 28th, 2014<br /> 
+      <nav><a href="html/">START</a></nav> 
+    </section>
+  </footer>
+
+</body>
+
+</html>
