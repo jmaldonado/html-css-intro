7e19577960631ab6dec2b3b55faa44decaed448a
root <root@nenasknittings.com>
2014-04-27 20:41:11 +0000

    Reworked the lesson plan a bit

--- index.php
+++ index.php
@@ -12,13 +12,29 @@
 
   <header id="body">
     <h1>Intro to HTML and CSS</h1>
-    <aside>Juan Maldonado, instructor</aside>
+    <aside>
+      Juan Maldonado, instructor<br />
+      Teaching demo for Portland Community College<br />
+      Rock Creek Campus - April 28th, 2014
+    </aside>
   </header>
+
+  <main>
+    <section id="main_content">
+    <h2>Lesson Objectives</h2>
+    <ol>
+      <li>Learn the history of HTML and CSS</li>
+      <li>Discuss what HTML and CSS do, and how they do it</li>
+      <li>Audience Participation: Guess That HTML Tag</li>
+      <li>How to create web pages using HTML and CSS</li>
+    </ol>
+    <p>Please hold all questions / applause until the end of the 15 minute lesson</p>
+    </section>
+  </main>
 
   <footer>
     <section id="footer_content">
-      Teaching demo for Portland Community College, Rock Creek Campus - April 28th, 2014<br /> 
-      <nav><a href="html/">START</a></nav> 
+      <nav><a href="html/">Let's begin</a></nav> 
     </section>
   </footer>
 
